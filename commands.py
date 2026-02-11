8c9568e2f060df92fcb2ebc811aed2143063aa56
Coder'Phantom95 <159680854+CoderRony955@users.noreply.github.com>
2026-02-11 22:18:06 +0530

    modified referwith commands

--- commands.py
+++ commands.py
@@ -57,12 +57,12 @@ class AllCommands:
             "for": "Quick share single package with your friends. By doing this Refer use to generate quick one click downloadable link of your package that you want's to refer, by doing this your friend can be able to download that package instantly directly from your system without single any issue",
             "example": "refer -path \'C:\\Users\\<name>\\Myprojects\\tool\'"
         },
-        "referwith -message \'your message\' -paths \'packages path\'": {
+        "referwith -message \'your message\' -paths \"path1\" \"path2\" \"path3\"": {
             "for": "Share multiple packages with a single proper message. By doing this it will creates a basic HTML webpage with proper message and downloadable links in structured manner",
-            "example": "referwith -message \'This is a GUI apps for xyz.\' -name \'C:\\Users\\<name>\\GUIApps\\gui_app1\\ C:\\Users\\<name>\\GUIApps\\gui_app2\\ \'"
+            "example": "referwith -message \'This is a GUI apps for xyz.\' -paths \"C:\\Users\\<name>\\CLITools\\cli_tool1\\\" \"C:\\Users\\<name>\\CLITools\\cli_tool2\\\""
         },
-        "referwith -template -message \'your message\' -paths \'packages path\'": {
+        "referwith -template -message \'your message\' -paths \"path1\" \"path2\" \"path3\"": {
             "for": "Share multiple packages in available webpage template. After running command refer display all available templates options, you can choose any one of them ",
-            "example": "referwith -template -message \'This is the cli tools for xyz.\' -paths \'C:\\Users\\<name>\\CLITools\\cli_tool1\\ C:\\Users\\<name>\\CLITools\\cli_tool2\\ \'"
+            "example": "referwith -template -message \'This is the cli tools for xyz.\' -paths \"C:\\Users\\<name>\\CLITools\\cli_tool1\\\" \"C:\\Users\\<name>\\CLITools\\cli_tool2\\\""
         },
     }
